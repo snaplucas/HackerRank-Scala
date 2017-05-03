e2028f54f890f4756cedeb982182288697c770db
Lucas Rodrigues <lucas.chapola@gmail.com>
2017-05-03 20:20:48 -0300

    +Introduction - filter array

--- src/main/scala/main/Introduction.scala
+++ src/main/scala/main/Introduction.scala
@@ -6,15 +6,15 @@ object Introduction {
     println(sumOdds2(List(3, 2, 4, 6, 5, 7, 8, 0, 1)))
   }
 
+  def listReplication1(num: Int, arr: List[Int]): List[Int] = {
+    arr.flatMap(x => repeatN(num, x, Nil))
+  }
+
   def repeatN(num: Int, item: Int, acc: List[Int]): List[Int] = {
     num match {
       case 0 => acc
       case _ => repeatN(num - 1, item, item :: acc)
     }
-  }
-
-  def listReplication1(num: Int, arr: List[Int]): List[Int] = {
-    arr.flatMap(x => repeatN(num, x, Nil))
   }
 
   def listReplication2(num: Int, arr: List[Int]): List[Int] = for {e <- arr; _ <- 1 to num} yield e
@@ -29,5 +29,15 @@ object Introduction {
   }
 
   def sumOdds2(arr: List[Int]): Int = arr.filter(a => a % 2 == 1).sum
+
+  def filterArray1(delim: Int, arr: List[Int]): List[Int] = arr.filter(a => a < delim)
+
+  def filterArray2(delim: Int, arr: List[Int]): List[Int] = for (e <- arr if e < delim) yield e
+
+  def filterArray3(delim: Int, arr: List[Int]): List[Int] =
+    if (arr.isEmpty) List()
+    else if (arr.head < delim) List(arr.head) ::: filterArray3(delim, arr.tail)
+    else filterArray3(delim, arr.tail)
+
 
 }
