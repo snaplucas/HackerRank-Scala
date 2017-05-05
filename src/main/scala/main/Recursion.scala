6dca9848ff3adf0d8c4e4bcf26d93e2e7be0e16e
Lucas Rodrigues <lucas.chapola@gmail.com>
2017-05-04 22:17:10 -0300

    +Recursion - fibonacci

--- src/main/scala/main/Recursion.scala
+++ src/main/scala/main/Recursion.scala
@@ -2,7 +2,16 @@ package main
 
 object Recursion {
 
-  def gcd(x: Int, y: Int): Int =
-    if (y == 0) x else gcd(y, x % y)
+  def main(args: Array[String]): Unit = {
+    println(fibonacci(5))
+  }
 
+  def gcd(x: Int, y: Int): Int = if (y == 0) x else gcd(y, x % y)
+
+  def fibonacci(x: Int): Int = {
+    if (x == 1) 0
+    else if (x == 2) 1
+    else fibonacci(x - 1) + fibonacci(x - 2)
+
+  }
 }
