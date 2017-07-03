a240fb2ebe9ca0bb8fc5f12941bf9931bf249b26
snaplucas <lucas.chapola@gmail.com>
2017-07-02 21:19:15 -0300

    +Strings: gem stones (incompleto)

--- src/main/scala/algorithms/Strings.scala
+++ src/main/scala/algorithms/Strings.scala
@@ -4,9 +4,11 @@ object Strings {
 
   def main(args: Array[String]) {
 
-    println(if (hackerRankInAString("hacakaeararanaka".toList)) "YES" else "NO")
-    println(if (hackerRankInAString("hhhhaaaaackkkkerrrrrrrrank".toList)) "YES" else "NO")
-    println(if (hackerRankInAString("crackerhackerknar".toList)) "YES" else "NO")
+    //    println(if (hackerRankInAString("hacakaeararanaka".toList)) "YES" else "NO")
+    //    println(if (hackerRankInAString("hhhhaaaaackkkkerrrrrrrrank".toList)) "YES" else "NO")
+    //    println(if (hackerRankInAString("crackerhackerknar".toList)) "YES" else "NO")
+
+    println(gemStones(List("abcd".toList, "abef".toList, "abjj".toList)))
   }
 
   val indices = Vector('h', 'a', 'c', 'k', 'e', 'r', 'r', 'a', 'n', 'k')
@@ -20,6 +22,11 @@ object Strings {
 
     hackerrank(s, 0)
   }
+
+  def gemStones(stones: List[List[Char]]) =
+    for {stone <- stones
+         gem <- stone
+         if stones.forall(a => a.forall(b => b == gem))} yield gem
 
   def makingAnagrams(a: String, b: String) = {}
 }
