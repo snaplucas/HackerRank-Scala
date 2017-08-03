cfda69e3ef939ce532edb635f3d720a0ffcb50c4
snaplucas <lucas.chapola@gmail.com>
2017-08-02 22:08:30 -0300

    +Recursion: sequence full of colors

--- src/main/scala/data/structures/Arrays.scala
+++ src/main/scala/data/structures/Arrays.scala
@@ -2,7 +2,24 @@ package data.structures
 
 object Arrays {
 
+  def main(args: Array[String]) {
+    println(if (sequenceFullColors("RYBG")) "True" else "False")
+  }
+
   def arraysDS(arr: Array[Int]) = arr.reverse.foreach(a => print(a + " "))
+
+  def sequenceFullColors(balls: String) = {
+    def sequence(ballsList: List[Char], redGreen: Int, yellowBlue: Int): Boolean = {
+      if (ballsList.isEmpty) redGreen == 0 && yellowBlue == 0
+      else if (ballsList.head == 'R') redGreen <= 1 && sequence(ballsList.tail, redGreen + 1, yellowBlue)
+      else if (ballsList.head == 'G') redGreen <= 1 && sequence(ballsList.tail, redGreen - 1, yellowBlue)
+      else if (ballsList.head == 'Y') yellowBlue <= 1 && sequence(ballsList.tail, redGreen, yellowBlue + 1)
+      else if (ballsList.head == 'B') yellowBlue <= 1 && sequence(ballsList.tail, redGreen, yellowBlue - 1)
+      else false
+    }
+
+    sequence(balls.toList, 0, 0)
+  }
 
   def sparseArrays() = {}
 }
