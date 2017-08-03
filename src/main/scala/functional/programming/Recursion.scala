0474384a281ecdf85efa81fd20417919c9e6e832
snaplucas <lucas.chapola@gmail.com>
2017-08-03 20:11:37 -0300

    Moving sequenceFullColors to correct class

--- src/main/scala/functional/programming/Recursion.scala
+++ src/main/scala/functional/programming/Recursion.scala
@@ -3,6 +3,8 @@ package functional.programming
 object Recursion {
 
   def main(args: Array[String]): Unit = {
+    println(if (sequenceFullColors("RYBG")) "True" else "False")
+
     prefixCompression("abcdefpr", "abcpqr")
 
     println(stringCompression("aaabaaaaccaaaaba".toList, 0))
@@ -86,5 +88,18 @@ object Recursion {
     }
 
     compression(x.toList, y.toList, StringBuilder.newBuilder)
+  }
+
+  def sequenceFullColors(balls: String) = {
+    def sequence(ballsList: List[Char], redGreen: Int, yellowBlue: Int): Boolean = {
+      if (ballsList.isEmpty) redGreen == 0 && yellowBlue == 0
+      else if (ballsList.head == 'R') redGreen <= 1 && sequence(ballsList.tail, redGreen + 1, yellowBlue)
+      else if (ballsList.head == 'G') redGreen <= 1 && sequence(ballsList.tail, redGreen - 1, yellowBlue)
+      else if (ballsList.head == 'Y') yellowBlue <= 1 && sequence(ballsList.tail, redGreen, yellowBlue + 1)
+      else if (ballsList.head == 'B') yellowBlue <= 1 && sequence(ballsList.tail, redGreen, yellowBlue - 1)
+      else false
+    }
+
+    sequence(balls.toList, 0, 0)
   }
 }
