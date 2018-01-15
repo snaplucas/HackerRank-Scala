799da383e7b5a004b244e78dfc8451baab5ac4fa
snaplucas <lucas.chapola@gmail.com>
2018-01-14 22:19:02 -0200

    +Implementation: staircase

--- src/main/scala/algorithms/Implementation.scala
+++ src/main/scala/algorithms/Implementation.scala
@@ -6,6 +6,18 @@ object Implementation {
     kangoroo(43, 2, 70, 2)
   }
 
+  def staircase(n: Int): Unit = {
+    def stair(n: Int, acc: Int): Unit = {
+      if (acc == 0) println('#'.toString * n)
+      else {
+        println(' '.toString * acc + '#'.toString * (n - acc))
+        stair(n, acc - 1)
+      }
+    }
+
+    stair(n, n - 1)
+  }
+
   def taumAndBday(b: Long, w: Long, x: Long, y: Long, z: Long): Long = {
     val yy = if (x + z <= y) x + z else y
     val xx = if (y + z <= x) y + z else x
