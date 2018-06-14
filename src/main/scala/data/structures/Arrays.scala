78352b402b9242b7b03bc6ef864df7a4c58dfcb8
snaplucas <lucas.chapola@gmail.com>
2018-06-13 23:29:32 -0300

    +dataStructures: leftRotation

--- src/main/scala/data/structures/Arrays.scala
+++ src/main/scala/data/structures/Arrays.scala
@@ -2,6 +2,19 @@ package data.structures
 
 object Arrays {
 
+  def main(args: Array[String]): Unit = {
+    val stdin = scala.io.StdIn
+    val a = stdin.readLine.split(" ").map(_.trim.toInt)
+    //     val rotations = if (n > s.size) n % s.size else n
+    println(leftRotation(List(1, 2, 3, 4, 5), 4).toString())
+
+  }
+
 
   def arraysDS(arr: Array[Int]): Unit = arr.reverse.foreach(a => print(a + " "))
+
+  def leftRotation(s: List[Int], n: Int): List[Int] = {
+    if (n == 0) s
+    else leftRotation(s.tail ::: List(s.head), n - 1)
+  }
 }
